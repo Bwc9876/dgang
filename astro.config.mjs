24ce501d8c2752ca7440c57e98166a82d43198ae
Ben C <bwc9876@gmail.com>
2024-11-12 11:59:13 -0500

    Add vtbot and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,23 +2,27 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'Dear God, Another Nix/NixOS Guide',
-			social: {
-				github: 'https://github.com/Bwc9876/dgang',
-			},
-			sidebar: [
-				{
-					label: 'Chapters',
-					autogenerate: { directory: "chapters" }
-				},
-			],
-			customCss: [
-				"./src/styles/theme.css"
-			]
-		}),
-	],
+    site: "https://dgang.bwc9876.dev",
+    integrations: [starlight({
+        title: 'Dear God, Another Nix/NixOS Guide',
+        social: {
+            github: 'https://github.com/Bwc9876/dgang',
+        },
+        sidebar: [
+            {
+                label: 'Chapters',
+                autogenerate: { directory: "chapters" }
+            },
+        ],
+        components: {
+            Head: "./src/components/Head.astro"
+        },
+        customCss: [
+            "./src/styles/theme.css"
+        ]
+    }), sitemap()],
 });
