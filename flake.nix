eb8fd14a6c6ae9ba3a0ec4fed92e6b46799e2caf
Ben C <bwc9876@gmail.com>
2024-11-13 18:20:17 -0500

    Flake package

--- flake.nix
+++ flake.nix
@@ -14,10 +14,19 @@
       inherit system;
     };
   in {
+    packages.${system}.default = pkgs.buildNpmPackage {
+      name = "dgang";
+      version = "0.1.0";
+      src = ./.;
+      packageJSON = ./package.json;
+      npmDepsHash = "sha256-AWSrIstiQhz2wPb3T7Vn0uvpJ5EQf5uyHl0pA3Bntnk=";
+      installPhase = "cp -r dist/ $out";
+      nativeBuildInputs = with pkgs; [d2];
+      ASTRO_TELEMETRY_DISABLED = 1;
+    };
     devShells.${system}.default = pkgs.mkShell {
       packages = with pkgs; [
         d2
-        pnpm
         nodejs
       ];
     };
